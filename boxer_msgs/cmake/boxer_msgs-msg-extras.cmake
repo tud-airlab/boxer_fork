95aab47afab1cd076b185b332169dda44b5fe6be
Stefan Bonhof <s.d.bonhof@tudelft.nl>
2021-02-15 14:19:27 +0100

    Initial commit with boxer packages from kinetic (added CMakeLists.txt)

---
+++ boxer_msgs/cmake/boxer_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(boxer_msgs_MESSAGE_FILES "msg/Drive.msg;msg/Lift.msg;msg/LiftPosition.msg;msg/MCUResetReason.msg;msg/MotorFeedback.msg;msg/Pid.msg;msg/SafetyMode.msg;msg/Status.msg;msg/TractionStatus.msg")
+set(boxer_msgs_SERVICE_FILES "")
