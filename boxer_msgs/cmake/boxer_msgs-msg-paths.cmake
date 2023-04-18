6e9883fc92836bc71a91b2a96a855dab6bee62b1
Chadi Salmi <c.salmi@tudelft.nl>
2023-04-18 09:06:46 +0200

    add these guys back

---
+++ boxer_msgs/cmake/boxer_msgs-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
+
+_prepend_path("${boxer_msgs_DIR}/.." "msg" boxer_msgs_MSG_INCLUDE_DIRS UNIQUE)
+set(boxer_msgs_MSG_DEPENDENCIES std_msgs)
